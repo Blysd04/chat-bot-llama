f7fd5637010b09e5664bd8846c90ec9ea45b4f67
Blysd04 <vannga1510@gmail.com>
2025-09-16 21:00:05 +0700

    update chat 2

---
+++ resources/js/pages/Chat-Components/ChatinputBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ChatinputBar({send, input, setInput}) {
+  return (
+    <div className='inputBar'>
+      <input className ="input" placeholder='Send a message...' type="text" value={input} onChange={(e)=>setInput(e.target.value)} onKeyDown={(e)=> { 
+        if (e.key==='Enter') 
+          send() 
+        }}/>
+      <button className='btn' onClick={send}>Send</button>
+    </div>
+  )
+}
